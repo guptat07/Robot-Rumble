bb1fafe9e5d0e2a1496651f7612ceaef98c8c7a4
singh-maya <singh.maya@ufl.edu>
2023-06-20 16:50:37 +0900

    updated level

--- main.py
+++ main.py
@@ -1,120 +1,38 @@
+"""
+Platformer Game
+"""
 import arcade
 
-SPRITE_SCALING = 0.5
+# Constants
+SCREEN_WIDTH = 1000
+SCREEN_HEIGHT = 650
+SCREEN_TITLE = "Platformer"
 
-SCREEN_WIDTH = 800
-SCREEN_HEIGHT = 600
-SCREEN_TITLE = "Move Sprite with Keyboard Example"
+# Constants used to scale our sprites from their original size
+CHARACTER_SCALING = 2
+TILE_SCALING = 4
+SPRITE_PIXEL_SIZE = 32
+GRID_PIXEL_SIZE = SPRITE_PIXEL_SIZE * TILE_SCALING
 
-TILE_SCALING = 0.5
-
-MOVEMENT_SPEED = 5
-RIGHT_FACING = 0
-LEFT_FACING = 1
-CHARACTER_SCALING = 0.3
-FRAMES_PER_SECOND = 60
-
+# Movement speed of player, in pixels per frame
+PLAYER_MOVEMENT_SPEED = 10
 GRAVITY = 1
+PLAYER_JUMP_SPEED = 20
 
+PLAYER_START_X = 50
+PLAYER_START_Y = 1000
 
-def load_texture_pair(filename):
-    """
-    Load a texture pair, with the second being a mirror image.
-    """
-    return [
-        arcade.load_texture(filename),
-        arcade.load_texture(filename, flipped_horizontally=True)
-    ]
+VIEWPORT_MARGIN_TOP = 60
+VIEWPORT_MARGIN_BOTTOM = 60
+VIEWPORT_RIGHT_MARGIN = 270
+VIEWPORT_LEFT_MARGIN = 270
 
+# How fast the camera pans to the player. 1.0 is instant.
+CAMERA_SPEED = 0.1
 
-class Player(arcade.Sprite):
-    """ Player Class """
-
-    def __init__(self):
-
-        # Set up parent class
-        super().__init__()
-
-        # Default to face-right
-        self.cur_time_frame = 0
-        self.character_face_direction = RIGHT_FACING
-
-        # Used for flipping between image sequences
-        self.cur_texture = 0
-
-        self.scale = CHARACTER_SCALING
-
-        # Load textures
-        self.idle_r = [1]
-        self.idle_l = [1]
-        self.running_r = [1]
-        self.running_l = [1]
-
-        for i in range(2):
-            texture_r = arcade.load_texture("sprites/Robot_idle.png", x=i * 1000, y=0, width=1000, height=1000)
-            texture_l = arcade.load_texture("sprites/Robot_idle.png", x=i * 1000, y=0, width=1000, height=1000,
-                                            flipped_horizontally=True)
-            self.idle_r.append(texture_r)
-            self.idle_l.append(texture_l)
-
-        for i in range(8):
-            texture_r = arcade.load_texture("sprites/Robot_run.png", x=i * 1000, y=0, width=1000, height=1000)
-            texture_l = arcade.load_texture("sprites/Robot_run.png", x=i * 1000, y=0, width=1000, height=1000,
-                                            flipped_horizontally=True)
-            self.running_r.append(texture_r)
-            self.running_l.append(texture_l)
-
-    def update_animation(self, delta_time):
-        # frames per second -> 60
-        self.cur_time_frame += delta_time
-        print("change x: ", self.change_x)
-        print("cur_time_frame time: ", self.cur_time_frame)
-
-        if self.change_x == 0 and self.change_y == 0:
-            if self.cur_time_frame >= 1 / 4:
-                self.texture = self.idle_r[self.idle_r[0]]
-                if self.idle_r[0] >= len(self.idle_r) - 1:
-                    self.idle_r[0] = 1
-                else:
-                    self.idle_r[0] = self.idle_r[0] + 1
-                self.cur_time_frame = 0
-                return
-
-        if self.change_x > 0:
-            if self.cur_time_frame >= 8 / 60:
-                self.texture = self.running_r[self.running_r[0]]
-                if self.running_r[0] >= len(self.running_r) - 1:
-                    self.running_r[0] = 1
-                else:
-                    self.running_r[0] = self.running_r[0] + 1
-                self.cur_time_frame = 0
-
-        if self.change_x < 0:
-            if self.cur_time_frame >= 8 / 60:
-                self.texture = self.running_l[self.running_l[0]]
-                if self.running_l[0] >= len(self.running_l) - 1:
-                    self.running_l[0] = 1
-                else:
-                    self.running_l[0] = self.running_l[0] + 1
-                self.cur_time_frame = 0
-
-    def update(self):
-        """ Move the player """
-        # Move player.
-        # Remove these lines if physics engine is moving player.
-        self.center_x += self.change_x
-        self.center_y += self.change_y
-
-        # Check for out-of-bounds
-        if self.left < 0:
-            self.left = 0
-        elif self.right > SCREEN_WIDTH - 1:
-            self.right = SCREEN_WIDTH - 1
-
-        if self.bottom < 0:
-            self.bottom = 0
-        elif self.top > SCREEN_HEIGHT - 1:
-            self.top = SCREEN_HEIGHT - 1
+LAYER_NAME_FOREGROUND = "Foreground"
+LAYER_NAME_BACKGROUND = "Background"
+LAYER_NAME_PLATFORMS = "Platforms"
 
 
 class MyGame(arcade.Window):
@@ -122,52 +40,46 @@ class MyGame(arcade.Window):
     Main application class.
     """
 
-    def update_player_speed(self):
+    def __init__(self):
 
-        # Calculate speed based on the keys pressed
-        self.player.change_x = 0
+        # Call the parent class and set up the window
+        super().__init__(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
 
-        if self.left_pressed and not self.right_pressed:
-            self.player.change_x = -MOVEMENT_SPEED
-        elif self.right_pressed and not self.left_pressed:
-            self.player.change_x = MOVEMENT_SPEED
-
-    def __init__(self, width, height, title):
-        """
-        Initializer
-        """
-
-        # Call the parent class initializer
-        super().__init__(width, height, title)
-
-        # Variables that will hold sprite lists
-        self.player_list = None
-
-        # Set up the player info
-        self.player = None
-        self.left_pressed = False
-        self.right_pressed = False
-        self.up_pressed = False
-        self.down_pressed = False
         # Our TileMap Object
+
         self.tile_map = None
 
-        # Set the background color
-        arcade.set_background_color(arcade.color.AMAZON)
+        # Our Scene Object
+        self.scene = None
+
+        # Separate variable that holds the player sprite
+        self.player_sprite = None
+
+        # Our physics engine
+        self.physics_engine = None
+
+        # A Camera that can be used for scrolling the screen
+        self.camera = None
+
+        # A Camera that can be used to draw GUI elements
+        self.gui_camera = None
+
+        self.end_of_map = 0
+        self.top_of_map = 0
+        self.end_of_map = 0
+
+        self.view_bottom = 0
+        self.view_left = 0
+
+        self.camera_sprites = arcade.Camera(SCREEN_WIDTH, SCREEN_HEIGHT)
+        self.camera_gui = arcade.Camera(SCREEN_WIDTH, SCREEN_HEIGHT)
 
     def setup(self):
-        """ Set up the game and initialize the variables. """
+        """Set up the game here. Call this function to restart the game."""
 
-        # Sprite lists
-        self.player_list = arcade.SpriteList()
-
-        self.player = Player()
-
-        self.player.center_x = SCREEN_WIDTH // 2
-        self.player.center_y = SCREEN_HEIGHT // 2
-        self.player.scale = 0.3
-
-        self.player_list.append(self.player)
+        # Set up the Cameras
+        self.camera = arcade.Camera(self.width, self.height)
+        self.gui_camera = arcade.Camera(self.width, self.height)
 
         # Name of map file to load
         map_name = "assets/Prototype.json"
@@ -175,18 +87,43 @@ class MyGame(arcade.Window):
         # Layer specific options are defined based on Layer names in a dictionary
         # Doing this will make the SpriteList for the platforms layer
         # use spatial hashing for detection.
+
         layer_options = {
             "Platforms": {
                 "use_spatial_hash": True,
             },
         }
-
         # Read in the tiled map
+
         self.tile_map = arcade.load_tilemap(map_name, TILE_SCALING, layer_options)
 
         # Initialize Scene with our TileMap, this will automatically add all layers
         # from the map as SpriteLists in the scene in the proper order.
+
         self.scene = arcade.Scene.from_tilemap(self.tile_map)
+
+        # Add Player Spritelist before "Foreground" layer. This will make the foreground
+        # be drawn after the player, making it appear to be in front of the Player.
+        # Setting before using scene.add_sprite allows us to define where the SpriteList
+        # will be in the draw order. If we just use add_sprite, it will be appended to the
+        # end of the order.
+        self.scene.add_sprite_list_after("Player", LAYER_NAME_FOREGROUND)
+
+        # Set up the player, specifically placing it at these coordinates.
+        image_source = "assets/robot_series_base_pack/robot1/robot1.png"
+        self.player_sprite = arcade.Sprite(image_source, CHARACTER_SCALING)
+        self.player_sprite.center_x = PLAYER_START_X
+        self.player_sprite.center_y = PLAYER_START_Y
+        self.scene.add_sprite("Player", self.player_sprite)
+
+        # --- Load in a map from the tiled editor ---
+
+        # Calculate the right edge of the my_map in pixels
+        self.end_of_map = self.tile_map.width * GRID_PIXEL_SIZE
+
+        # Calculate the right edge of the my_map in pixels
+        self.top_of_map = self.tile_map.height * GRID_PIXEL_SIZE
+        self.end_of_map = self.tile_map.width * GRID_PIXEL_SIZE
 
         # --- Other stuff
         # Set the background color
@@ -195,64 +132,70 @@ class MyGame(arcade.Window):
 
         # Create the 'physics engine'
         self.physics_engine = arcade.PhysicsEnginePlatformer(
-            self.player_sprite, gravity_constant=GRAVITY, walls=self.scene["Platforms"]
+            self.player_sprite,
+            gravity_constant=GRAVITY,
+            walls=self.scene[LAYER_NAME_PLATFORMS],
         )
 
     def on_draw(self):
-        """
-        Render the screen.
-        """
+        """Render the screen."""
 
-        # This command has to happen before we start drawing
+        # Clear the screen to the background color
         self.clear()
 
-        # Draw all the sprites.
-        self.player_list.draw()
+        # Activate the game camera
+        self.camera.use()
 
         # Draw our Scene
         self.scene.draw()
 
-    def on_update(self, delta_time):
-        """ Movement and game logic """
-
-        # Move the player
-        self.player_list.update_animation()
+        # Activate the GUI camera before drawing GUI elements
+        self.gui_camera.use()
 
     def on_key_press(self, key, modifiers):
-        """Called whenever a key is pressed. """
+        """Called whenever a key is pressed."""
 
-        if key == arcade.key.W:
-            self.up_pressed = True
-            self.update_player_speed()
-        elif key == arcade.key.S:
-            self.down_pressed = True
-            self.update_player_speed()
-        elif key == arcade.key.A:
-            self.left_pressed = True
-            self.update_player_speed()
-        elif key == arcade.key.D:
-            self.right_pressed = True
-            self.update_player_speed()
+        if key == arcade.key.UP or key == arcade.key.W:
+            if self.physics_engine.can_jump():
+                self.player_sprite.change_y = PLAYER_JUMP_SPEED
+                arcade.play_sound(self.jump_sound)
+        elif key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = -PLAYER_MOVEMENT_SPEED
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = PLAYER_MOVEMENT_SPEED
 
     def on_key_release(self, key, modifiers):
+        """Called when the user releases a key."""
 
-        if key == arcade.key.W:
-            self.up_pressed = False
-            self.update_player_speed()
-        elif key == arcade.key.S:
-            self.down_pressed = False
-            self.update_player_speed()
-        elif key == arcade.key.A:
-            self.left_pressed = False
-            self.update_player_speed()
-        elif key == arcade.key.D:
-            self.right_pressed = False
-            self.update_player_speed()
+        if key == arcade.key.LEFT or key == arcade.key.A:
+            self.player_sprite.change_x = 0
+        elif key == arcade.key.RIGHT or key == arcade.key.D:
+            self.player_sprite.change_x = 0
+
+    def center_camera_to_player(self):
+        screen_center_x = self.player_sprite.center_x - (self.camera.viewport_width / 2)
+        screen_center_y = self.player_sprite.center_y - (self.camera.viewport_height / 2)
+        if screen_center_x < 0:
+            screen_center_x = 0
+        if screen_center_y < 0:
+            screen_center_y = 0
+        player_centered = screen_center_x, screen_center_y
+
+        self.camera.move_to(player_centered)
+
+    def on_update(self, delta_time):
+        """Movement and game logic"""
+
+        # Move the player with the physics engine
+        self.physics_engine.update()
+
+        # Position the camera
+        self.center_camera_to_player()
 
 
 def main():
-    """ Main function """
-    window = MyGame(SCREEN_WIDTH, SCREEN_HEIGHT, SCREEN_TITLE)
+    """Main function"""
+    window = MyGame()
     window.setup()
     arcade.run()
 
