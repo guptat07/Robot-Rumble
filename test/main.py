b74b1de1d72980aefb2892a3317196cae54f818a
aaliao <wiseanthonyliao@gmail.com>
2023-06-22 18:30:36 +0900

    more boss gun stuff

--- test/main.py
+++ test/main.py
@@ -27,10 +27,17 @@ class MyGame(arcade.Window):
         self.platform_list = None
         self.bullet_list = None
 
+        self.bullet_list_p = None
         # player info
         self.player = None
         self.test_sprite = None
         self.timer = 0
+        self.health = 10
+        self.form_swap_timer = 0
+
+        #WILL MOVE INTO BOSS IN FIRST REFACTOR
+        self.first_form = True
+
 
         # Our physics engine
         self.physics_engine = None
@@ -48,6 +55,10 @@ class MyGame(arcade.Window):
         # Move the center of the player sprite to match the mouse x, y
         self.test_sprite.center_x = x
         self.test_sprite.center_y = y
+        print("vals:")
+        print(x)
+        print(y)
+        print()
     def setup(self):
         #TEST
         img = ":resources:images/animated_characters/female_person/femalePerson_idle.png"
@@ -86,6 +97,12 @@ class MyGame(arcade.Window):
         self.scene.add_sprite_list("player_list")
         self.bullet_list = arcade.SpriteList()
 
+        self.bullet_list_p = arcade.SpriteList()
+
+        self.scene.add_sprite_list("bullet_list_p")
+
+        self.scene.add_sprite_list("bullet_list")
+
         # initialize player
         self.player = boss()
         self.player.center_x = constants.SCREEN_WIDTH // 2
@@ -96,7 +113,6 @@ class MyGame(arcade.Window):
         #bullet test
 
         #bullet ring
-        self.bullet_list_p =[]
         for i in range(0,360,60):
             x = projectile(100,constants.BULLET_RADIUS, self.player.center_x, self.player.center_y, 0,0,i)
             y = projectile(100,constants.BULLET_RADIUS+100, self.player.center_x, self.player.center_y, 0,0, i+30)
@@ -132,10 +148,10 @@ class MyGame(arcade.Window):
 
         # Draw our Scene
         self.scene.draw(filter=gl.NEAREST)
-        self.bullet_list.draw()
+        #self.bullet_list.draw()
 
         #testing HITBOX/PATH
-        #self.scene.draw_hit_boxes(color=[255,255,255],names=["Boss","Platforms"])
+        #self.scene.draw_hit_boxes(color=[255,255,255],names=["Boss","Platforms",])
         #arcade.draw_arc_outline(self.player.center_x, self.player.center_y,2*constants.BULLET_RADIUS,2*constants.BULLET_RADIUS,arcade.color.WHITE,0,360)
 
         # Activate the GUI camera before drawing GUI elements
@@ -147,36 +163,75 @@ class MyGame(arcade.Window):
     def on_update(self, delta_time):
         """ Movement and game logic """
 
+        boss_col = arcade.check_for_collision_with_list(self.test_sprite,
+                                                                 self.player_list)
+
+        for boss in boss_col:
+            print("hit somethin!")
+            self.health = self.health - 1
+            print(self.health)
+
         platform_hit_list = arcade.check_for_collision_with_list(self.player,
                                                                  self.platform_list)
-        for platform in platform_hit_list:
+
+        bullet_collisions = arcade.check_for_collision_with_list(self.test_sprite,
+                                                                 self.bullet_list)
+
+        for bull in bullet_collisions:
             print("hit somethin!")
+            bull.remove_from_sprite_lists()
+            self.health = self.health - 1
+            print(self.health)
+
+        bullet_collisions_p = arcade.check_for_collision_with_list(self.test_sprite,
+                                                                 self.bullet_list_p)
+
+        for bull in bullet_collisions_p:
+            print("hit somethin!")
+            bull.remove_from_sprite_lists()
+            self.health = self.health - 1
+            print(self.health)
+
+        for platform in platform_hit_list:
+            pass
+            #print("hit somethin!")
             #platform.remove_from_sprite_lists()
 
+        self.form_swap_timer = self.form_swap_timer + delta_time
+        if self.form_swap_timer >= constants.FORM_TIMER:
+            self.first_form = not self.first_form
+            self.form_swap_timer = 0
 
-        #bullet ring
-        for bullet in self.bullet_list_p:
-            bullet.pathing(self.player.center_x,self.player.center_y,delta_time)
+        if self.first_form:
+            self.bullet_list_p.visible = True
+            #bullet ring
+            for bullet in self.bullet_list_p:
+                bullet.pathing(self.player.center_x,self.player.center_y,delta_time)
 
-        #spawn homing bullets
+            #spawn homing bullets
 
-        self.timer = self.timer + delta_time
-        #print(len(self.bullet_list))
-        for bullet in self.bullet_list:
-            #print("running")
-            bullet.homing(delta_time)
+            self.timer = self.timer + delta_time
+            #print(len(self.bullet_list))
+            for bullet in self.bullet_list:
+                #print("running")
+                bullet.homing(delta_time)
 
-        if self.timer >= 1:
-            x = projectile(100, 0, self.player.center_x, self.player.center_y, self.test_sprite.center_x,self.test_sprite.center_y,0)
-            self.bullet_list.append(x)
-            self.scene.add_sprite("bull", x)
-           #print("player stuff going in")
-            #print(self.player.center_x, self.player.center_y, self.test_sprite.center_x,self.test_sprite.center_y,sep=", ")
-            self.timer = 0
+            if self.timer >= 1:
+                x = projectile(100, 0, self.player.center_x, self.player.center_y, self.test_sprite.center_x,self.test_sprite.center_y,0)
+                self.bullet_list.append(x)
+                self.scene.add_sprite("bull", x)
+               #print("player stuff going in")
+                #print(self.player.center_x, self.player.center_y, self.test_sprite.center_x,self.test_sprite.center_y,sep=", ")
+                self.timer = 0
+
+        else:
+            self.player.boss_logic(delta_time)
+            self.bullet_list_p.visible = False
+            #print(self.bullet_list_p.visible)
 
 
-        self.player.update()
-        #self.player.boss_logic(delta_time)
+
+        self.player.update() #refactor this shit
         self.physics_engine.update()
         # Move the player
         self.player_list.update_animation()
