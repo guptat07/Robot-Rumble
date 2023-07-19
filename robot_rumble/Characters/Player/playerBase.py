c9d094d6385aeee7939abdef59f592013d4f2227
aaliao <wiseanthonyliao@gmail.com>
2023-07-19 17:54:41 +0900

    On god fr no cap

--- robot_rumble/Characters/Player/playerBase.py
+++ robot_rumble/Characters/Player/playerBase.py
@@ -23,7 +23,7 @@ class PlayerBase(Entity):
         self.character_face_direction = constants.RIGHT_FACING
 
         # Set health
-        self.health = 3
+        self.health = 20
         self.health_bar = PlayerHealthBar()
         self.death = Player_Death()
         self.is_alive = True
